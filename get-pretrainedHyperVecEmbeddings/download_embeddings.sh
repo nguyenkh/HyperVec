878d7b39f2953ed0567d61ca5d45c0163ba7078c
Kim Anh Nguyen <anhnk.ims@gmail.com>
2021-02-08 09:47:44 +0700

    fix the error links

--- get-pretrainedHyperVecEmbeddings/download_embeddings.sh
+++ get-pretrainedHyperVecEmbeddings/download_embeddings.sh
@@ -2,7 +2,7 @@
 # downloads hypvec_embeddings from IMS homepage
 for id in 0 1 2 3 4 5 6 7 8 9
 	do
-	wget http://www.ims.uni-stuttgart.de/forschung/ressourcen/experiment-daten/hypvec_embd/hyp_p${id}.gz
+	wget http://www.ims.uni-stuttgart.de/documents/ressourcen/experiment-daten/hypvec_embd/hyp_p${id}.gz
 	done
 cat hyp_p0.gz hyp_p1.gz hyp_p2.gz hyp_p3.gz hyp_p4.gz hyp_p5.gz hyp_p6.gz hyp_p7.gz hyp_p8.gz hyp_p9.gz > hypervec.txt.gz
 # rm -f hyp_p*.gz # OPTIONAL  -remove files
